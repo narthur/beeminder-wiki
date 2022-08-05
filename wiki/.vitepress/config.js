91be779e9135f1913c3a7b78f10f278cb7ac46ab
Nathan Arthur <nathan@nathanarthur.com>
2022-08-05 15:18:34 -0400

    logo

--- wiki/.vitepress/config.js
+++ wiki/.vitepress/config.js
@@ -3,6 +3,7 @@ import { defineConfig } from 'vitepress'
 export default defineConfig({
     title: 'Beeminder Wiki',
     themeConfig: {
+        logo: 'beeminder.png',
         editLink: {
             pattern: 'https://github.com/narthur/beeminder-wiki/edit/master/wiki/:path'
         }
