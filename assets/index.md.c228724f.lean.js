7cdc3745e0d29e6960dc394dff250f127655c8a8
dreeves <dreeves@users.noreply.github.com>
2022-08-08 23:13:08 +0000

    deploy: 77c0d7eed5c524bc31719c035f42728a89b1567c

---
+++ assets/index.md.c228724f.lean.js
@@ -0,0 +1,1 @@
+import{_ as e,c as t,o as r,a as i}from"./app.e960b7f1.js";const _=JSON.parse('{"title":"Beeminder Wiki","description":"","frontmatter":{},"headers":[],"relativePath":"index.md"}'),n={name:"index.md"},o=i("",5),a=[o];function s(h,c,d,p,u,m){return r(),t("div",null,a)}var b=e(n,[["render",s]]);export{_ as __pageData,b as default};
