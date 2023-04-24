f3dd98c43e8a69a3adb5df6cd3d925a54f06b140
narthur <narthur@users.noreply.github.com>
2023-04-24 17:29:00 +0000

    deploy: 23d7d67bb289f79b8d06356e6709f560c96cd31f

---
+++ assets/articles.md.625db8b2.lean.js
@@ -0,0 +1,1 @@
+import{_ as e,c as r,o as a,a as t}from"./app.a810b278.js";const f=JSON.parse('{"title":"Articles","description":"","frontmatter":{},"headers":[],"relativePath":"articles.md"}'),n={name:"articles.md"},o=t("",3),i=[o];function l(s,c,h,d,_,m){return a(),r("div",null,i)}var b=e(n,[["render",l]]);export{f as __pageData,b as default};
